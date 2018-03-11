f7dfb7a33377ac89d376b3c01839fe691f78a919
Henry Post <HenryFBP@gmail.com>
2018-03-10 19:48:34 -0600

    move server project to proper location

--- server/donationcoordinator/donationcoordinator/wsgi.py
+++ server/donationcoordinator/donationcoordinator/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for donationstation project.
+WSGI config for donationcoordinator project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "donationstation.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "donationcoordinator.settings")
 
 application = get_wsgi_application()
