011c7098576c80c2c7a4a41e72b30ae1ac6e9faa
Henry Post <HenryFBP@gmail.com>
2018-03-11 19:29:17 -0500

    finish 'sign up' page

--- server/donationcoordinator/donationcoordinator/views.py
+++ server/donationcoordinator/donationcoordinator/views.py
@@ -1,7 +1,9 @@
 import random
 
 from django.http import *
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -14,5 +16,16 @@ def index(request: HttpRequest):
     return render(request, 'index.html', context)
 
 
-def signup(request: HttpRequest):
-    return HttpResponse("oh u wanna sign up?")
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('root')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
