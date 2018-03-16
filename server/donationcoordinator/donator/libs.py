820cff25e7c0e4294bbd3dc65ed80b8968b6e8dc
Henry Post <HenryFBP@gmail.com>
2018-03-15 21:12:11 -0500

    begin dictToUL

--- server/donationcoordinator/donator/libs.py
+++ server/donationcoordinator/donator/libs.py
@@ -1,5 +1,6 @@
 # general-purpose library functions used by ``donator`` class.
 
+import copy
 import json
 from pprint import pprint
 
@@ -27,7 +28,7 @@ def itemToLI(item: str):
     safeName = item.replace(" ", "-")  # cannot have spaces in CSS classes
 
     ret += wrap(item, "label")  # add label
-    ret += wrap(None, "input", ["type", "name"], ["number", safeName]) # add input elt
+    ret += wrap(None, "input", ["type", "name"], ["number", safeName])  # add input elt
 
     ret = wrap(ret, "li", "class", safeName)  # wrap it in an <li>
 
@@ -43,18 +44,37 @@ def itemListToUL(items: list):
     for item in items:
         ret += itemToLI(item) + "\n"
 
-    ret = wrap("\n"+ret, "ul")
+    ret = wrap("\n" + ret, "ul")
+
+    return ret
+
+def dictToUL_rec(dictionary: dict, elts: list):
+    pass
+
+def dictToUL(dictionary: dict):
+    """Given any dictionary of groups of any depth, return an HTML <ul>
+    that represents that tree of items and item groups."""
+
+    d = copy.deepcopy(dictionary)
+    ret = ''
+
+    while d:
+        for key, value in d.copy().items():
+            print(key + ": " + str(value))
+            del d[key]
 
     return ret
 
 
 if __name__ == '__main__':
-    it = getItemTemplate("../static/data/items.json")
-    pprint(it)
+    d = getItemTemplate("../static/data/items.json")
+    pprint(d)
 
-    perishable = it['food']['perishable']  # list of perishable food
+    perishable = d['food']['perishable']  # list of perishable food
     pprint(perishable)
 
     perishableElt = itemListToUL(perishable)  # perishable food but as <ul> elt
-
     print("Perishable food list: \n" + perishableElt)
+
+    ulElt = dictToUL(d)
+    print("Entire <ul> elt:\n" + ulElt)
