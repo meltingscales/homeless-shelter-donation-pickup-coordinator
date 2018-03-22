497f1e6ce649f98bfd5f9da54da83d2ec3ca1a3b
Henry Post <HenryFBP@gmail.com>
2018-03-21 19:02:33 -0500

    add search for going through ItemsList

--- server/donationcoordinator/static/js/itemslist.js
+++ server/donationcoordinator/static/js/itemslist.js
@@ -18,7 +18,14 @@ function toggleClass(p, c)
 
 function toggleView(p)
 {
-    toggleClass(p, 'hidden')
+    toggleClass(p, 'hidden');
+}
+
+function removeAllClasses(c)
+{
+    $('.'+c).each(function(i, elt) {
+        $(this).removeClass(c);
+    });
 }
 
 var a_elts = $('section#items form ul a');
@@ -43,6 +50,55 @@ a_elts.each(function(i, elt) { //for all <a> tags
         }
 
     });
+});
+
+$(document).on('keyup', function(e) {
+    if(e.keyCode == 27)
+    {
+        removeAllClasses('hidden');
+        removeAllClasses('highlighted');
+        $('#items-search input').val('');
+    }
+});
+
+$('#items-search input').on('keyup', function(e) {
+    var val = $(this).val();
+
+    val = val.replace(' ','_');
+
+    if(val == "")
+    {
+        removeAllClasses('hidden');
+        removeAllClasses('highlighted');
+        return;
+    }
+
+    //show all as we will assume that all have what we are seeking
+    $('form li').each(function(i, elt) {
+        $(elt).addClass('hidden');
+    });
+
+    var s = 'li[class*='+val+']';
+
+    print("Selector: ");
+    print(s);
+
+    $(s).each(function (i, elt) {
+        print("match:");
+        print(this);
+
+        $(this).removeClass('hidden'); //shows elt we want
+
+        e = this.parentElement
+        while(e.nodeName !== "FORM") //bubble up repeatedly
+        {
+            $(e).removeClass('hidden');
+            $(e).addClass('highlighted');
+            e = e.parentElement;
+        }
+
+    });
+
 });
 
 });
