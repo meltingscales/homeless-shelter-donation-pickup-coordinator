09cdada77bfd7cb2def7c689300b0c1c2486b205
HenryFBP <HenryFBP@gmail.com>
2018-03-23 12:01:41 -0500

    turn on markdown support

--- server/donationcoordinator/org/models.py
+++ server/donationcoordinator/org/models.py
@@ -4,9 +4,16 @@ from django.db import models
 # Create your models here.
 from donator.models import User
 
+default_markdown_file = 'static/data/default_markdown.txt'
+default_markdown = ''
+
+with open(default_markdown_file, 'r') as f:
+    for line in f:
+        default_markdown += line + "\n"
+
 
 class Org(models.Model):
-    description = models.TextField(max_length=500, blank=True)
+    description = models.TextField(max_length=5000, blank=True, default=default_markdown)
     name = models.CharField(max_length=30, null=True, blank=True)
 
     def markdownify(self):
@@ -17,6 +24,6 @@ class Org(models.Model):
 
         if len(userQS) is 0:
             return None
-        
+
         user = userQS[0]  # get user from queryset
         return user.username
