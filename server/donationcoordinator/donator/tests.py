bbae3f91b5fd17cadf375555e4ff43b793968f66
HenryFBP <HenryFBP@gmail.com>
2018-04-06 13:27:41 -0500

    make output files look like auto-generated files
    by prepending underscore

--- server/donationcoordinator/donator/tests.py
+++ server/donationcoordinator/donator/tests.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from django.test import TestCase
@@ -10,8 +11,14 @@ from donator.libs import ItemList
 class TestItemList(TestCase):
 
     def test_itemlist(self):
+        # location of base datafile that dictates structure of all other files
         itemsLoc = os.path.join(settings.PROJECT_ROOT, "static/data/items.json")
-        htmlloc = os.path.join(settings.PROJECT_ROOT, 'static/data/items_autogen.html')
+
+        # output for donator items html
+        html_out = os.path.join(settings.PROJECT_ROOT, 'static/data/_items.html')
+
+        # output for donator items data that's stored
+        items_out = os.path.join(settings.PROJECT_ROOT, 'static/data/_items_donator.json')
 
         print(os.path.abspath(itemsLoc))
 
@@ -22,6 +29,10 @@ class TestItemList(TestCase):
         print("List of items ready for serializing into database:")
         print(itemList.data)
 
+        with open(items_out, 'w+') as file:
+            jsondata = json.dumps(itemList.data, indent=4, sort_keys=True)
+            file.write(jsondata)
+
         print("Form elt:")
         print(itemList.to_html())
 
@@ -30,5 +41,5 @@ class TestItemList(TestCase):
         print("List of items after we apply a simulated JSON form's POST data")
         print(itemList.data)
 
-        with open(htmlloc, 'w+') as file:  # open file to write test HTML
+        with open(html_out, 'w+') as file:  # open file to write test HTML
             file.write(itemList.to_html())
